9c96836aa579d5e0c2d35671e32fa9be5a288f6b
Melissa Martinez <melmar12@live.com>
2017-10-20 18:26:37 -0500

    lecture 88: todo and todo list components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
-
+var TodoList = require('TodoList');
 var TodoApp = React.createClass({
+	getInitialState: function () {
+		return {
+			todos: [
+			 	{ 
+					id: 1,
+					text: 'Walk the dog'
+				}, 
+				{
+					id: 2,
+					text: 'study for test'
+				},
+				{
+					id: 3,
+					text: 'cook lunch'
+				},
+				{
+					id: 4,
+					text: 'watch movie'
+				}
+			]
+		}
+	},
 	render: function () {
+		var {todos} = this.state;
+
+
 		return (
 			<div>
-				TodoApp.jsx
+				<TodoList todos={todos}/>
 			</div>
 		)
 	}
